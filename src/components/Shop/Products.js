5391a7825c48c19fcf2543201720b0f70ec7f22c
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-10-18 15:55:43 -0600

    Update cart-slice and dependencies
    - Implement removeItemFromCart function in cart-slice
    
    - Add dummy products in products.js
    
    - Implement dynamic product display in product.js

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "First Book",
+    description: "The first book I laid eyes on",
+  },
+  {
+    id: "p2",
+    price: 8,
+    title: "Old Book",
+    description:
+      "The oldest book found in the attic, it has a weird oxydated iron cover",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
